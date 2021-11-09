2e0a7ba83ce93759ea87249a10854cf81cd2f547
krzyww <markiewiczpio@gmail.com>
2021-11-09 21:14:56 +0100

    poczatek

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import pygame
 
-print("dupa")
+WIDTH, HEIGHT = 1024, 768
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+
+
+
+
+def main():
+
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        pygame.init()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+    main()
+
+
+if __name__ == "__main__":
+    main()
