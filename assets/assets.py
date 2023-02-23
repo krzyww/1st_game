ee4a3b5f5a862a4ddf7df29eaff6fdf3660abeec
krzyww <markiewiczpio@gmail.com>
2023-02-23 21:28:33 +0100

    pep8 formatting

--- assets/assets.py
+++ assets/assets.py
@@ -1,4 +1,6 @@
-import pygame, os
+import pygame
+import os
+
 
 class img:
     pygame.font.init()
@@ -13,15 +15,16 @@ class img:
     HIT_FONT = pygame.font.SysFont('consolas', 40)
 
     WIDTH, HEIGHT = 1200, 700
-    WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
     SHIP_WIDTH, SHIP_HIGHT = 80, 80
-    SHIP_IMAGE = pygame.image.load(os.path.join('assets','craft.png'))
+    SHIP_IMAGE = pygame.image.load(os.path.join('assets', 'craft.png'))
     SHIP = pygame.transform.scale(SHIP_IMAGE, (SHIP_WIDTH, SHIP_HIGHT))
 
     BULLET_WIDTH, BULLET_HEIGHT = 20, 20
-    BULLET_IMAGE = pygame.image.load(os.path.join('assets','bullet.png'))
-    BULLET = pygame.transform.scale(BULLET_IMAGE, (BULLET_WIDTH, BULLET_HEIGHT))
+    BULLET_IMAGE = pygame.image.load(os.path.join('assets', 'bullet.png'))
+    BULLET = pygame.transform.scale(
+        BULLET_IMAGE, (BULLET_WIDTH, BULLET_HEIGHT))
 
-    BKG_IMAGE = pygame.image.load(os.path.join('assets','background.png'))
+    BKG_IMAGE = pygame.image.load(os.path.join('assets', 'background.png'))
     BKG = pygame.transform.scale(BKG_IMAGE, (WIDTH, HEIGHT))
--- entities.py
+++ entities.py
@@ -1,5 +1,6 @@
 import pygame
-import random, os
+import random
+import os
 
 
 class DefaultEnemy:
@@ -7,10 +8,10 @@ class DefaultEnemy:
         self.width = 300
         self.height = 300
         self.image = pygame.image.load(os.path.join('assets','enemy.png'))
-        self.start_x = random.randint(1,1100)
-        self.start_y = random.randint(1,500)
+        self.start_x = random.randint(200, 1000)
+        self.start_y = random.randint(1, 300)
 
-    def aaa(self):
+    def calculations(self):
         self.enemy = pygame.transform.scale(self.image, (self.width, self.height))
         self.rect = pygame.Rect(self.start_x, self.start_y, self.width, self.height)
 
@@ -26,13 +27,14 @@ class SpawnSmallEnemy(DefaultEnemy):
         super().__init__()
         self.set_height(30)
         self.set_width(30)
-        self.aaa()
+        self.calculations()
+
 
 class SpawnRafal(DefaultEnemy):
     def __init__(self) -> None:
         super().__init__()
         self.set_height(60)
         self.set_width(60)
-        self.image = pygame.image.load(os.path.join('assets','enemy2.png'))
-        self.aaa()
+        self.image = pygame.image.load(os.path.join('assets', 'enemy2.png'))
+        self.calculations()
 
