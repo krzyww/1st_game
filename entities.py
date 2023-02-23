c08a2365092dfd57e3554cd7df0f8ad91a611f25
krzyww <markiewiczpio@gmail.com>
2023-02-23 22:18:12 +0100

    formatting

--- entities.py
+++ entities.py
@@ -7,13 +7,15 @@ class DefaultEnemy:
     def __init__(self) -> None:
         self.width = 300
         self.height = 300
-        self.image = pygame.image.load(os.path.join('assets','enemy.png'))
+        self.image = pygame.image.load(os.path.join('assets', 'enemy.png'))
         self.start_x = random.randint(200, 1000)
         self.start_y = random.randint(1, 300)
 
     def calculations(self):
-        self.enemy = pygame.transform.scale(self.image, (self.width, self.height))
-        self.rect = pygame.Rect(self.start_x, self.start_y, self.width, self.height)
+        self.enemy = pygame.transform.scale(
+            self.image, (self.width, self.height))
+        self.rect = pygame.Rect(
+            self.start_x, self.start_y, self.width, self.height)
 
     def set_height(self, value):
         self.height = value
@@ -37,4 +39,3 @@ class SpawnRafal(DefaultEnemy):
         self.set_width(60)
         self.image = pygame.image.load(os.path.join('assets', 'enemy2.png'))
         self.calculations()
-
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pygame
 import random
 from assets.assets import img
-from entities import *
+from entities import SpawnRafal, SpawnSmallEnemy
 
 
 def draw_window(player, enemies: list, bullets: list, hits: int):
-    img.WIN.blit(img.BKG,(0, 0))
+    img.WIN.blit(img.BKG, (0, 0))
     img.WIN.blit(img.SHIP, (player.x, player.y))
     for ene in enemies:
         img.WIN.blit(ene.enemy, (ene.rect.x, ene.rect.y))
@@ -13,28 +13,36 @@ def draw_window(player, enemies: list, bullets: list, hits: int):
         img.WIN.blit(img.BULLET, bul)
 
     aaa = img.HIT_FONT.render("Hits: " + str(hits), 1, img.WHITE)
-    img.WIN.blit(aaa,(0,0))
+    img.WIN.blit(aaa, (0, 0))
     pygame.display.update()
 
+
 def player_movement(keys_pressed, player):
-    if keys_pressed[pygame.K_LEFT] and player.x > 0:     #left
+    # left
+    if keys_pressed[pygame.K_LEFT] and player.x > 0:
         player.x -= img.VEL
-    if keys_pressed[pygame.K_RIGHT] and player.x < img.WIDTH-img.SHIP_WIDTH:     #right
-        player.x += img.VEL       
-    if keys_pressed[pygame.K_DOWN] and player.y < img.HEIGHT-img.SHIP_HIGHT:     #down
+    # right
+    if keys_pressed[pygame.K_RIGHT] and player.x < img.WIDTH-img.SHIP_WIDTH:
+        player.x += img.VEL
+    # down
+    if keys_pressed[pygame.K_DOWN] and player.y < img.HEIGHT-img.SHIP_HIGHT:
         player.y += img.VEL
-    if keys_pressed[pygame.K_UP] and player.y > 0:     #up
+    # up
+    if keys_pressed[pygame.K_UP] and player.y > 0:
         player.y -= img.VEL
+
 
 def enemy_movement_1(enemies: list):
     for ene in enemies:
-        ene.rect.x += random.randint(-5,5)
-        ene.rect.y += random.randint(-5,6)
+        ene.rect.x += random.randint(-5, 5)
+        ene.rect.y += random.randint(-5, 6)
+
 
 def handle_bullets(bullets: list, enemies: list):
     for bul in bullets:
         bul[1] = int(bul[1]) - img.BULLET_VEL
-        bullet = pygame.Rect(bul[0], bul[1], img.BULLET_WIDTH, img.BULLET_HEIGHT)
+        bullet = pygame.Rect(
+            bul[0], bul[1], img.BULLET_WIDTH, img.BULLET_HEIGHT)
         for ene in enemies:
             if bul[1] < 0:
                 try:
@@ -53,11 +61,11 @@ def handle_bullets(bullets: list, enemies: list):
 
 
 def main():
-    player = pygame.Rect(img.WIDTH/2, img.HEIGHT*3/4, img.SHIP_WIDTH, img.SHIP_HIGHT)    
+    player = pygame.Rect(img.WIDTH/2, img.HEIGHT*3/4,
+                         img.SHIP_WIDTH, img.SHIP_HIGHT)
     bullets = []
     enemies = [SpawnSmallEnemy()]
     hits = 0
- 
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -68,7 +76,9 @@ def main():
                 run = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL:
-                    img_bull_position = [player.x + img.SHIP_WIDTH//2 - img.BULLET_WIDTH//2, player.y]
+                    img_bull_position = [
+                        player.x + img.SHIP_WIDTH//2 - img.BULLET_WIDTH//2,
+                        player.y]
                     bullets.append(img_bull_position)
             if event.type == img.HIT:
                 hits += 1
